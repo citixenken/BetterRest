711214a139f4e79a14304d6343a2628797891a83
citixenken <kmuyesu@gmail.com>
2021-12-19 12:19:10 +0300

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ken Muyesu on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
