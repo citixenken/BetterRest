3b1b4b8b1e18054315cc794e7ba8be3916a82c73
citixenken <kmuyesu@gmail.com>
2021-12-20 23:36:49 +0300

    prepping basic layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,15 +10,22 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            
-            DatePicker("Enter a date: ", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
-                .labelsHidden() //allows screen readers to use it for voiceovers
-            
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
-                .padding(20)
+        NavigationView {
+            VStack {
+                Text(Date.now, format: .dateTime.day().month().year())
+                
+                DatePicker("Enter a date: ", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+                    .labelsHidden() //allows screen readers to use it for voiceovers
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
+                    .padding(20)
+                
+                Stepper("\(coffeeAmount.formatted()) cups", value: $coffeeAmount, in: 0...12, step: 1)
+                    .padding(20)
+            }
         }
         
     }
